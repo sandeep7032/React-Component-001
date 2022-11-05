e2e7b929e273911445f5489de1a3bf6c814b672e
sandeep7032 <95558185+sandeep7032@users.noreply.github.com>
2022-11-05 20:43:52 +0530

    Add files via upload

---
+++ src/components/Profile.jsx
@@ -0,0 +1,18 @@
+
+const Profile = () => {
+    return (
+        <div>
+            <h1>
+                Chitigori Sandeep Kumar
+            </h1>
+            <h3>
+                Aspiring Software Developer, Student at Geekster.
+            </h3>
+            <a href="https://www.linkedin.com/in/sandeep-chitigori/">Linkedin</a>
+            &nbsp;
+            <a href="https://github.com/sandeep7032">Github</a>
+        </div>
+    );
+};
+
+export default Profile;

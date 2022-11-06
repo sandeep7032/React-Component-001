8b0cf5c96d45c1422e8d8d2bbee35f60332c4ef2
sandeep7032 <95558185+sandeep7032@users.noreply.github.com>
2022-11-06 17:26:17 +0530

    Add files via upload

---
+++ src/components/Hobbies.jsx
@@ -0,0 +1,16 @@
+const Hobbies = () => {
+    return (
+        <div>
+            <h2>
+                Hobbies
+            </h2>
+            <ol>
+                <li>Playing Cricket</li>
+                <li>Playing Chess</li>
+                <li>Watching Movies</li>
+            </ol>
+        </div>
+    )
+};
+
+export default Hobbies;
